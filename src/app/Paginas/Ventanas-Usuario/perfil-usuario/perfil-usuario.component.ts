a27c358bd9186404a3953590464e5f8882ddf4c2
neitorxd <diego.molina@mayor.cl>
2021-04-27 00:11:34 -0500

    Version Estable v1.0

--- src/app/Paginas/Ventanas-Usuario/perfil-usuario/perfil-usuario.component.ts
+++ src/app/Paginas/Ventanas-Usuario/perfil-usuario/perfil-usuario.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilUsuarioComponent implements OnInit {
 
-  constructor() { }
+  nombredeusuario:String = "Nombre de usuario"
+  Nombreyapellido:String = "John Doe"
+  Direccion:String = "Avenida siempre viva 107"
+  Numerodeventas:Number = 0
+
+  constructor() {
+    this.Numerodeventas = 123;
+   }
 
   ngOnInit(): void {
   }
